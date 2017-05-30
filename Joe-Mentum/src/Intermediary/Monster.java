62b52de4c29dbbf27bd29623babad6af067e1a26
cBachoo <cosmin_cb@hotmail.com>
2017-05-30 13:25:07 -0400

    FIXED THE BULLSHIT THAT WAS THAT MERGE CONFLICT
    I DESERVE A COOKIE FOR THIS, OR A SHINY NICKEL.

---
+++ Joe-Mentum/src/Intermediary/Monster.java
@@ -0,0 +1,44 @@
+/*
+Name: Quinn Fisher
+Date Created: May 26th, 2017
+Date Modified: May 26th, 2017
+Description: The class used to handle enemy AI
+ */
+
+package Intermediary;
+
+import Logic.LivingObject;
+
+public class Monster extends LivingObject {
+
+	private static final char RUSH = 'r';
+	private static final char WANDER = 'w';
+	private Player Joe;
+	private Monster nextnode;
+	private char aiState;
+	
+	public Monster(int health, int attack, double maxSpeed, char AIstate) {
+		this.setHealth(health);
+		this.setAttack(attack);
+		this.setSpeed(maxSpeed);
+		this.aiState = AIstate;
+	}
+	
+	//AIState setter/getter (May 26th, 2017)
+	public void setAIState(char newState) { aiState = newState; }
+	public char getAIState() { return aiState ; }
+	
+	
+	public void behave() {
+		switch(aiState) {
+		case RUSH:
+			boolean direction = true;
+			if (Joe.getX() < this.getX()) //TODO Update w/ actual Joe object
+				direction = false;
+			this.moveSide(direction);
+			break;
+		case WANDER:
+			break;
+		}
+	}
+}
---
+++ Joe-Mentum/src/Intermediary/Player.java
@@ -0,0 +1,26 @@
+/*
+Name: Quinn Fisher
+Date Created: May 25th, 2017
+Date Modified: May 25th, 2017
+Description: The class used to handle the player's stats and motion
+ */
+
+package Intermediary;
+
+import Logic.LivingObject;
+
+public class Player extends LivingObject {
+
+	private int EXP = 0;
+	private int level = 1;
+	//private Item passiveItems;
+	//private Item activeItem;
+	
+	public Player() {
+		//TODO Update Values
+		this.setAttack(0);
+		this.setHealth(0);
+		this.setSpeed(0);
+	}
+	
+}

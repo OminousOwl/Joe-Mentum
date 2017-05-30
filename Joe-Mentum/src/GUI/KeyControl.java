ddafe16cb38a7c25082221c6b6c45c0e7867013f
OlivierJHebert <jarrod1999@gmail.com>
2017-05-30 08:31:06 -0400

    The Keyboard input manage

---
+++ Joe-Mentum/src/GUI/KeyControl.java
@@ -0,0 +1,46 @@
+package GUI;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+/*
+ *Project: ICS4U - Final Project - Joe-Mentum
+ *Author: Olivier J Hébert
+ *Date Created: 29/05/2017
+ *Date Edited: 29/05/2017
+ *Description: This is the interface for the keyboard that handles all player input.
+ */
+public class KeyControl implements KeyListener {
+	/**
+	 *Title:
+	 * Input Manager
+	 *Author:
+	 * Olivier J Hébert
+	 *Date Created:
+	 * 29/05/2017
+	 *Date Edited:
+	 * 29/05/2017
+	 *Parameters:
+	 * N/A
+	 *Dependencies: 
+	 * Keylistener
+	 *Exceptions:
+	 * N/A
+	 *Description: this object is a listener that manages all keyboard input
+	 */
+	private void inputManager(KeyEvent e){
+		if(e.equals('w') || e.equals(' ')){//Player jumps when spacebar/the 'w' key is pressed
+			Player.jump();
+		}//end if
+		if(e.equals('d')){//Player moves right when the 'd' key is pressed
+			moveSide(Player.getSpeed() > 0);
+		}//end if
+		if(e.equals('a')){//Player moves left when the 'a' key is pressed
+			moveSide(Player.getSpeed() > 0);
+		}//end if
+		if(e.equals('s')){
+			Player.pickupItem();
+		}//end if
+		if(e.equals('e')){
+			//use item
+		}//end if
+	}//end inputManager
+}//end class

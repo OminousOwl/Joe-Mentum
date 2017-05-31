04789363227aac383045336bea1d90bb2d7adf15
QFisher <quinnfisher@hotmail.com>
2017-05-31 08:23:58 -0400

    Temp push to avoid potential merge conflicts

--- Joe-Mentum/src/GUI/MainGame.java
+++ Joe-Mentum/src/GUI/MainGame.java
@@ -1,8 +1,23 @@
 package GUI;
 
 import java.awt.event.*;
+import java.util.EventListener;
 
-public class MainGame implements ActionListener {
+import javax.swing.JFrame;
+
+import Intermediary.Player;
+import Logic.Entity;
+import Logic.LivingObject;
+
+/*
+ *       __
+ * 	 ___( o)> quack
+ *   \ <_. )
+ *    `---' 
+ */
+
+public class MainGame extends JFrame implements Runnable, EventListener {
+	
 	/**** Constants ****/
 	private static final int RUNNING = 0;//the ID# for the game's running state.
 	private static final int PAUSED_MENU = 1;//the ID# for the game's paused state with the basic menu.
@@ -10,12 +25,49 @@ public class MainGame implements ActionListener {
 	
 	/**** Variables ****/
 	private static int state = RUNNING;//the flag that triggers different behaviors in the program
+	public static final Player Joe = new Player(); //The man, the myth, the legend himself, Joe
+
+	//TODO get this POS out of our code
+	public static void main(String[] args) { 
+		MainGame test = new MainGame();
+	}
 	
-	public static void setState(int newState){
+	public MainGame() {
+		//Sets up the game panel
+		setSize(500, 500);
+		Entity floor = new Entity();
+		floor.x = 300;
+		floor.height = 200;
+		floor.y = 0;
+		floor.width = 500;
+		setVisible(true);
+	}
+	
+
+	
+	public void run() {
+		
+	}
+	
+	/*
+	Name: gravity
+	Description: Adjusts the ySpeed of an object to cause it to fall
+	Parameters: One LivingObject
+	Return Value/Type: N/A
+	Dependencies: Logic.LivingObject
+	Exceptions: N/A
+	Date Created: May 29th, 2017 (Created in another class)
+	Date Modified: May 29th, 2017
+	 */
+	public void gravity(LivingObject e) {
+		e.setYSpeed(e.getYSpeed() + LivingObject.GRAV);
+	}
+	
+	
+	public void setState(int newState){
 		state = newState;
 	}//end setState
 	
-	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("Back")){
 			state = state-1;
@@ -29,4 +81,5 @@ public class MainGame implements ActionListener {
 			//terminate game
 		}//end if
 	}//end actionPerformed
+
 }//end class

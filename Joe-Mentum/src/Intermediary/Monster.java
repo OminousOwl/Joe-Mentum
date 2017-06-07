23cd8154899c0acbfeb793163539600ee8922b69
cBachoo <cosmin_cb@hotmail.com>
2017-06-07 08:37:28 -0400

    Revert "Created initial Wander AI (No player detection)"
    This reverts commit 347c2b9f74f43aea29cd6aecc982d0a22202cc86.

--- Joe-Mentum/src/Intermediary/Monster.java
+++ Joe-Mentum/src/Intermediary/Monster.java
@@ -1,13 +1,11 @@
 /*
 Name: Quinn Fisher
 Date Created: May 26th, 2017
-Date Modified: June 5th, 2017
+Date Modified: May 26th, 2017
 Description: The class used to handle enemy AI
  */
 
 package Intermediary;
-
-import java.util.Random;
 
 import Logic.LivingObject;
 
@@ -15,15 +13,9 @@ public class Monster extends LivingObject {
 
 	private static final char RUSH = 'r';
 	private static final char WANDER = 'w';
-	private static final char AgWANDER = 'g';
-	private static final char RUN = 'n';
-	private static final char RtWANDER = 't';
-	private static final char BIRD = 'b';
-	
+	private Player Joe;
 	private Monster nextnode;
 	private char aiState;
-	
-	private int moveFrames;
 	
 	public Monster(int health, int attack, double maxSpeed, char AIstate) {
 		this.setHealth(health);
@@ -38,77 +30,15 @@ public class Monster extends LivingObject {
 	
 	
 	public void behave() {
-		boolean direction = true;
 		switch(aiState) {
 		case RUSH:
+			boolean direction = true;
 			if (Joe.getX() < this.getX()) //TODO Update w/ actual Joe object
 				direction = false;
 			this.moveSide(direction);
 			break;
-			
-		//True wandering, no player detection, 100% passive
 		case WANDER:
-			if (moveFrames > 0) { //If the entity still has frames left to move
-				if (Math.abs(this.getXSpeed()) > 0) { //Move frames in progress
-					moveSide(direction);
-				} //Alternative: Do nothing (ergo no code)
-			}
-			else { //New action should be called
-				if (this.getXSpeed() == 0) {
-					Random rnd = new Random();
-					direction = rnd.nextBoolean();
-					moveSide(direction);
-					moveFrames = randNumber(10, 120);
-				}
-				else { //Time to stop moving!
-					this.setXSpeed(0);
-					moveFrames = randNumber(10, 120);
-				}
-			}
-			break;
-		case AgWANDER:
-			
-			break;
-		case RUN:
-			if (Joe.getX() > this.getX()) //TODO Update w/ actual Joe object
-				direction = false;
-				this.moveSide(direction);
-			break;
-		case RtWANDER:
-			break;
-		case BIRD:
 			break;
 		}
-	}
-	
-	/*
-	Name: wander
-	Description: Causes a monster to aimlessly roam
-	Parameters: None
-	Return Value/Type: N/A
-	Dependencies: None
-	Exceptions: N/A
-	Date Created: June 5th, 2017
-	Date Modified: June 5th, 2017
-	 */
-	public void wander() {
-		if (this.getXSpeed() == 0 && moveFrames <= 0) {
-			
-			
-		}
-	}
-	
-	/*
-	 Name: randNumber
-	 Description: Generates a random number
-	 Parameters: Two Integers
-	 Return Value/Type: A random number within the range of the parameters
-	 Dependencies: Java.Math
-	 Creation Date: October 23rd, 2015
-	 Modification Date: April 13th, 2017
-	 Throws: None
-	*/
-	public static int randNumber(int min, int max) {
-		return min + (int) (Math.random() * ((max - min) + 1));
 	}
 }

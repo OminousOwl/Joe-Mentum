88dbb185675f846dae0928aaa835efe60535ca21
QFisher <quinnfisher@hotmail.com>
2017-06-17 21:12:11 -0400

    Started on item system

--- Joe-Mentum/src/Logic/Item.java
+++ Joe-Mentum/src/Logic/Item.java
@@ -3,12 +3,22 @@ package Logic;
  *Project: ICS4U - Final Project - Joe-Mentum
  *Author: Olivier J Hébert
  *Date Created: 26/05/2017
- *Date Edited: 26/05/2017
+ *Date Edited: 17/06/2017
  *Description: This class will serve as the node for the item database.
  */
 public class Item extends Entity {
 	/**** Variables ****/
-	public boolean isPassive;//the flag to determine the type of item (consumable/equipment)
-	public char effect;//the reference to the list of item effects
-	public String description;//a description of an item's effect
+	private boolean isActive;//the flag to determine the type of item (consumable/equipment)
+	private char effect;//the reference to the list of item effects
+	private String description;//a description of an item's effect
+	
+	private int cooldown;
+	private int cdRemaining;
+	
+	
+	public void use() {
+		if (isActive && cdRemaining == 0) {
+			
+		}
+	}
 }
